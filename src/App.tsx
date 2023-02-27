022180d1a352078d995cfcc69898f36c4b6e17c3
jeff0518 <cycivs07@icloud.com>
2023-02-27 16:43:18 +0800

    feat: Initialize Project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <p className="bg-red-500 text-white">Hello Vite + React + Tailwind CSS!</p>
+    </div>
+  )
+}
+
+export default App
